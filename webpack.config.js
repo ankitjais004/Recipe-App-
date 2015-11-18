04fa0948858d2280ec9141b7c7a236a0f78ec19c
Ankit Jaiswal <ajaiswal@kreeti.com>
2015-11-18 14:59:18 +0530

    Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
-var webpack = require('webpack');
 var path = require('path');
+var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH,'components');
 var BUILD_PATH = path.resolve(ROOT_PATH,'build');
-var merge = require('webpack-merge');
-var TARGET = process.env.npm_lifecycle_event;
 
-module.exports = {
+process.env.BABEL_ENV = TARGET;
+
+var common = {
   entry: './components/sample.js',
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js', '.jsx']
   },
-  output:{
-    path: BUILD_PATH,
-    filename: 'bundle.js'
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
   },
-  devServer: {
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Recipe App'
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
@@ -24,19 +42,8 @@ module.exports = {
       host: process.env.HOST,
       port: process.env.PORT
     },
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        loaders: ['babel'],
-        include: APP_PATH
-      }
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  plugin: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlwebpackPlugin({
-      title: 'Recipe App'
-    })
-  ]
-};
+  });
+}

0d5ba306931a0e61acd52d9f5728b0fad66cbd42
Ankit Jaiswal <ajaiswal@kreeti.com>
2015-11-18 11:32:24 +0530

    Checking webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,48 @@
 var webpack = require('webpack');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ROOT_PATH = path.resolve(__dirname);
+//var merge = require('webpack-merge');
+//var Target = process.env.npm_lifecycle_event;
 
 module.exports = {
-  entry:
+  entry: '/components/sample.js'
+  resolve: {
+    extensions: ['', '.js']
+  },
   output:{
+    path: path.resolve(ROOT_PATH, 'build')
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['babel'],
+        exclude: node_modules,
+        include: path.resolve(ROOT_PATH, 'components')
+      }
+    ]
+  }
   plugin: [
-    new Htmlwe
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Recipe App'
+    })
   ]
-}
+};
+
+// if(TARGET === 'start' || !TARGET) {
+//   module.exports = merge(common, {
+//     devtool: 'eval-source-map',
+//     devServer: {
+//       historyApiFallback: true,
+//       hot: true,
+//       inline: true,
+//       progress: true
+//     },
+//     plugins: [
+
+//     ]
+//   });
+// }

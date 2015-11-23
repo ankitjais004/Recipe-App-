d21a33fab2f4f7d44d3c060894ae3724d4392233
Ankit Jaiswal <ajaiswal@kreeti.com>
2015-11-23 11:11:07 +0530

    Recipe List and Recipe form is ok

--- components/Recipe.jsx
+++ components/Recipe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Recipe extends React.Component {
   render() {
     return(
-      <li> {this.props.item}
+      <li> <a href="#"> {this.props.item} </a>
       </li>
     )
   }
--- components/UserPage.jsx
+++ components/UserPage.jsx
@@ -7,44 +7,56 @@ import RecipeList from "./RecipeList.jsx";
 export default class UserPage extends React.Component {
   constructor(props){
     super(props);
-    this.state = ({ 'flag': 2 , "RecipeBox" : [] });
+    this.state = ({ 'flag': 2 , "RecipeBox" : [], "Button": "Create New Recipe" });
     this.handleClick = this.handleClick.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   };
 
-  handleClick() {
+  handleClick(e) {
+    e.preventDefault();
     this.setState({ flag : this.state.flag + 1 });
-    this.setState({ showForm : 1 });
+    var elem = ReactDOM.findDOMNode(this.refs.createRecipeButtonRef).value;
+    console.log(elem);
+    if(elem === "Create New Recipe")
+    {
+      this.setState({Button: "Cancel"})
+    }
+    else {
+      this.setState({Button: "Create New Recipe"})
+    }
   };
 
   handleSubmit(RecipeName) {
     var newBox = this.state.RecipeBox.slice();
     newBox.push(RecipeName);
+
     this.setState({RecipeBox: newBox})
+
   };
 
   render() {
-    var createRecipe;
+    var createRecipe , NoList = '';
     if( this.state.flag % 2 === 1){
       createRecipe =   <CreatingRecipe onSubmitRecipe = {RecipeName => this.handleSubmit(RecipeName)} />
-      //document.querySelector("#createRecipeButton").val = "Dismiss Recipe";
+
+    };
+    if (this.state.RecipeBox.length === 0 ){
+      NoList = "Empty List";
     }
-    //else if( this.state.flag % 2 === 0){
-     // createRecipe = null;
-//      document.querySelector(".RecipeList").innerHTML = "Create New Recipe";
-    //}
 
     return (
       <div>
-        <span id="headings"><h1> Welcome to Your Profile </h1></span>
-        <div className="RecipeList">
-          <RecipeList RecipeItems = {this.state.RecipeBox} />
-        </div>
-        <div className="createRecipeButtonAndFormBinder">
-        <input type="button" onClick= { this.handleClick } value="Create New Recipe" id="createRecipeButton" />
-        { createRecipe }
-        </div>
+      <span id="header"><h1> Welcome to Your Profile </h1></span>
+      <div className="RecipeList">
+      <RecipeList RecipeItems = {this.state.RecipeBox} />
+      {NoList}
+      </div>
+      <div className="createRecipeButtonAndFormBinder">
+      <input type="button" onClick= {this.handleClick } value={this.state.Button} id="createRecipeButton" ref="createRecipeButtonRef" />
+      { createRecipe }
+      </div>
       </div>
     );
   }
+
 }
